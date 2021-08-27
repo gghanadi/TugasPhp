d515e1e909a73401130aab24f86bd49fddcfcc61
gghanadi <57656144+gghanadi@users.noreply.github.com>
2021-08-27 14:45:50 +0700

    Create tugasPhp.php
    disini saya akan share tentang online test. bagaiman program 8 bidak saling memakan dan dapat di print disini saya  menggunakan bahasa php

---
+++ tugasPhp.php
@@ -0,0 +1,86 @@
+<?php
+$hasilX = [];
+$hasilY = [];
+$finalX =[];
+$finalY =[];
+    for($i=1;$i<=8;$i++){
+        $x = rand(1,8);
+        $y = rand(1,8);
+        echo "masukan bidak $i". "\n";
+        if($i==1){
+            $x=2;
+            $y=1;
+            echo $x.",".$y. "\n";
+            $hasilX[] = $x;
+            $hasilY[] = $y;
+        }else{
+            echo $x.",".$y. "\n";
+            $hasilX[] = $x;
+            $hasilY[] = $y;
+        }
+        
+    }
+
+    //hasil di sini jika jalan yang di hitung satu persatu jadi bidak 1 di bandingkan dengan bidak 2
+
+    // for($x=0;$x<=7;$x++){
+    //     if($x%2==0){
+    //         continue;
+    //     }
+    //     else if($x+1>7){
+    //         break;
+    //     }
+    //     else if($hasilY[$x]==$hasilY[$x+1]){
+    //         $finalX[]=$hasilX[$x];
+    //         $finalY[]=$hasilY[$x];
+    //     }else if($hasilX[$x]!=$hasilX[$x+1]){
+    //         $check1 =$hasilY[$x]-$hasilY[$x+1];
+    //         if($check1==0 || $check1==1 || $check1==-1){
+    //             $finalX[]=$hasilX[$x];
+    //             $finalY[]=$hasilY[$x];
+    //         }
+
+    //     }else if($x==6){
+    //         break;
+    //     }
+    //     else{
+    //         continue;
+    //     }
+    // }
+    // if($finalX==null || $finalX==0){
+    //     echo "tidak ditemukan";
+    // }else{
+    //     var_dump($finalX);
+    //     echo "\n";
+    //     var_dump($finalY);
+    // }
+
+    //ini jika bidak 1 di bandingkan dengan semua bidak terlebih dahulu
+    for($x=0;$x<=7;$x++){
+        for($y=$x+1;$y<=7;$y++){
+            if($hasilY[$x]==$hasilY[$y]){
+                $finalX[]=$hasilX[$y];
+                $finalY[]=$hasilY[$y];
+            }else if($hasilX[$x]==$hasilX[$y]){
+                $check1 =$hasilY[$x]-$hasilY[$y];
+                if($check1==0 || $check1==1 || $check1==-1){
+                    $finalX[]=$hasilX[$y];
+                    $finalY[]=$hasilY[$y];
+                }else{
+                    continue 2;
+                }
+    
+            }
+        }
+    }
+    echo "Hasil : \n";
+    if($finalX==null || $finalX==0){
+        echo "tidak ditemukan";
+    }else{
+        $countFinal = count($finalX);
+        for($z=0;$z<$countFinal;$z++){
+            echo "($finalX[$z],$finalY[$z])";
+        }
+    }
+
+?>
